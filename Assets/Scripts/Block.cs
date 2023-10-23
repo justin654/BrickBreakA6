0436e9f29365df1bd39eedd524ce1463c3ec402c
justin654 <justin654@gmail.com>
2023-10-23 02:26:12 -0400

    EXPLOSIOOOON
    Set up a little script that instantiates some fragments, and adds force when blocks are broken. This looks a lot nicer in my opinion than the particle system I created.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -9,26 +9,53 @@ public class Block : MonoBehaviour
     public int points = 100;
     private GameSession gameSession;
     private int timesHit;
-    private int hitsRemaining;
+    private SpriteRenderer spriteRenderer;
+
 
     [Header("Block Properties")]
     [SerializeField] private int blockHitsToDestroy = 1;
+    [SerializeField] private int hitsRemaining;
+
     [SerializeField] private bool unbreakable = false;
+
+    [Header("Color Properties")]
+    [SerializeField] private Color breakableColor = Color.white;
+    [SerializeField] private Color unbreakableColor = Color.red;
+
+    [Header("Explode Properties")]
+    public bool explodeOnDestroy;
+    public GameObject fragmentPrefab;
+    public int numberOfFragments = 5;
+    public float delayTime = 0.1f;
+
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        timesHit = 0;
-        hitsRemaining = blockHitsToDestroy;
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
         audioSource = GetComponent<AudioSource>();
-
         gameSession = FindObjectOfType<GameSession>();
+
         if (gameSession != null)
         {
             gameSession.RegisterBlock(!unbreakable);
         }
+
+        if (unbreakable)
+        {
+            spriteRenderer.color = unbreakableColor;
+        }
+        else
+        {
+            spriteRenderer.color = breakableColor;
+        }
+
+
+        timesHit = 0;
+        hitsRemaining = blockHitsToDestroy;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -47,6 +74,10 @@ public class Block : MonoBehaviour
             audioSource.PlayOneShot(hitSound);
         }
     }
+    public int GetHitsRemaining()
+    {
+        return hitsRemaining;
+    }
 
     private void DestroyBlock()
     {
@@ -60,10 +91,24 @@ public class Block : MonoBehaviour
         {
             gameSession.BlockDestroyed(points);
         }
-
+        Explode();
         Destroy(gameObject, hitSound.length);
 
     }
 
+    private void Explode()
+    {
+        for (int i = 0; i < numberOfFragments; i++)
+        {
+            GameObject fragment = Instantiate(fragmentPrefab, transform.position, Quaternion.Euler(0f, 0f, Random.Range(0f, 360f)));
 
+            Rigidbody2D rb = fragment.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                Vector2 randomForce = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized * Random.Range(2f, 5f);
+                rb.AddForce(randomForce, ForceMode2D.Impulse);
+            }
+            Destroy(fragment, delayTime);
+        }
+    }
 }
