d58ccd2c05ffaa8e2136354cbb35ff179d6d872f
justin654 <justin654@gmail.com>
2023-10-22 22:16:55 -0400

    Add GameOver scene to Build
    Added GameOver scene to Build Settings and added the swap to that scene when your ball triggers floor. Also read that you should CompareTage instead of accessing tags directly so here we are on line 16.

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Floor : MonoBehaviour
 {
+    private SceneLoader sceneLoader;
+
+    private void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Ball")
+        if (collision.gameObject.CompareTag("Ball"))
         {
             Debug.Log("You hit the floor and lost, transition to gameover");
+            sceneLoader.LoadGameOverScene();
         }
     }
 }
